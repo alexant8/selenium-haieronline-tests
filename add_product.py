4b824e9b6fb4ab7de4ecae21ce170b98e9de6ecc
Alexey <128811132+alexant8@users.noreply.github.com>
2026-02-13 15:06:41 +0300

    Add files via upload

--- add_product.py
+++ add_product.py
@@ -1,10 +1,11 @@
-import driver
 import pytest
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from urllib3.util import wait
+
 
 class TestAddproduct:
     """Тесты добавления в корзину"""
@@ -44,22 +45,35 @@ class TestAddproduct:
         auth_button = driver.find_element(By.XPATH, "/html/body/div[26]/div[2]/div/div/div/div[2]/form/div[4]")
         auth_button.click()#Клик по кнопке авторизации
 
+        """Тестирование добавления товара в корзину"""
+
         catalog_button = driver.find_element(By.XPATH, "/html/body/div[6]/div[1]/header/div/div[2]/button[2]")
         catalog_button.click()#Открытие каталога
 
         climate_button = driver.find_element(By.XPATH, "/html/body/div[6]/div[1]/header/div/div[2]/div[4]/div/div/div/div/div[1]/ul/li[5]/a")
         climate_button.click()#Открытие раздела "Климатическия техника"
 
+        WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "/html/body/div[6]/div[1]/main/div[1]/main/div/div[4]/div[2]/div/div[2]/div/div[1]/div[2]/div/div/div[1]/div[1]/div[1]/div/div[2]/div[2]/button[1]"))
+        )
+
         driver.execute_script("window.scrollBy(0, 300);")  # скролл на 300 пикселей вниз
 
         add_button = driver.find_element(By.XPATH, "/html/body/div[6]/div[1]/main/div[1]/main/div/div[4]/div[2]/div/div[2]/div/div[1]/div[2]/div/div/div[1]/div[1]/div[1]/div/div[2]/div[2]/button[1]")
         add_button.click()#Добавить товар в корзину
 
-        time.sleep(3)
+        WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[6]/div[1]/main/div[1]/main/div[2]/div/div/div[2]/div[3]/a/button"))
+        )
 
         redirect_button = driver.find_element(By.XPATH, "/html/body/div[6]/div[1]/main/div[1]/main/div[2]/div/div/div[2]/div[3]/a/button")
         redirect_button.click()#Перейти в корзину
 
-        delete_product_button = driver.find_element(By.XPATH, "/html/body/div[5]/div[1]/main/div[1]/main/div/div[2]/div[1]/section/div/div/div/div[1]/div[1]/div[2]/button/svg/path[2]")
-        delete_product_button.click()#Удалить товар из корзины
 
+        WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[5]/div[1]/main/div[1]/main/div/div[2]/div[2]/aside/div/div[3]/button"))
+        )
+
+        order_button = driver.find_element(By.XPATH, "/html/body/div[5]/div[1]/main/div[1]/main/div/div[2]/div[2]/aside/div/div[3]/button")
+        order_button.click()#Нажать оформить заказ
